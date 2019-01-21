418eae525fae6149184fdd465aa1901d5cd5160e
Kareem Ali <kareem.adel.000@gmail.com>
2019-01-20 19:05:51 -0800

    Re-read the file with each request to detect changes

--- backend_weather/index.js
+++ backend_weather/index.js
@@ -4,13 +4,12 @@ const express = require('express');
 const { join } = require('path');
 
 const { port } = require(join(__dirname, 'config.js'));
-const data = require(join(__dirname, 'sampleData.js'));
 
 const app = express();
 
-app.get('/forecast', (req, res) => {
+app.get('/forecast', async (req, res) => {
   res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.send(data);
+  res.sendFile(join(__dirname, 'sampleData.json'));
 });
 
 app.listen(port, () => {
